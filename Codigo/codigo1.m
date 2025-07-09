fbe2bc7b45d10f5b9fb8cab94724f97793bf97e3
Nicolas Piñera <nicolaspinera@gmail.com>
2025-07-08 21:52:43 -0300

    Actualizacion de los diagramas

--- Codigo/codigo1.m
+++ Codigo/codigo1.m
@@ -13,7 +13,7 @@ s=tf('s');
 Ta = 25; # Temperatura ambiente
 Tfinal = 300; # Temperatura del Horno en regimen estacionario
 potencia_aplicada = 1000;  # [W]
-Rt = (Tfinal-Ta)/potencia_aplicada
+Rt = (Tfinal-Ta)/potencia_aplicada;
 
 c = 1005; # Calor especifico del aire 1005 J/kg
 V = 0.067; # Volumen del horno 0.067 m^3
@@ -24,20 +24,22 @@ C = m * c;
 K = Rt;
 tau = C * Rt;
 
-G = K / (tau*s+1)
+G = K / (tau*s+1);
 #step(G);
 Ksensor = 0.01
 Krele = 200
+Has = 1.5/(0.0318*s+1);
 
-FTLA = G * Ksensor * Krele;
+FTLA = G * Ksensor * Krele * Has;
 #step(FTLA,250)
 FTLA
 
 Gtotal = G * Krele;
+Htotal = Ksensor * Has;
 
 
-FTLC = Gtotal / (1+Gtotal*Ksensor);
-# step(FTLC,250);
+FTLC = Gtotal / (1+Gtotal*Htotal);
+step(FTLC);
 FTLC
 
 disp('Terminado');
